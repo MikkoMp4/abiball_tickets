7bfdb4916fd2b3bdd37aaebc8131a1174b6b64bd
Mikko.mp4 <84526459+MikkoMp4@users.noreply.github.com>
2026-04-28 23:43:04 +0200

    Configure CORS with dynamic allowed origin

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ const app = express();
 // Trust reverse-proxy headers (needed when running behind nginx/traefik in Docker)
 app.set('trust proxy', 1);
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGIN || false,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
